b576b5e9e123689694bb334a5f0b9ab5ccd2df6c
Meet Sundrani <meetsundrani@gmail.com>
2024-12-19 20:37:30 +0530

    added validations in  form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,69 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const name = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   }
 
+  const handleButtonClick = () => {
+    debugger
+    const error = checkValidData(email.current.value, password.current.value, name?.current?.value, !isSignInForm);
+    setErrorMessage(error);
+  }
+
   return (
     <div>
-      <Header/>
-      <div className='absolute'>
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/8e8ff9d3-17b4-4ce1-b571-06ba4e025cca/web_collection/IN-en-20241125-TRIFECTA-649c9db8-a195-4f78-a2e6-f4a02bda21f6_large.jpg" alt="logo" />
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/8e8ff9d3-17b4-4ce1-b571-06ba4e025cca/web_collection/IN-en-20241125-TRIFECTA-649c9db8-a195-4f78-a2e6-f4a02bda21f6_large.jpg"
+          alt="logo"
+        />
       </div>
-      <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 align-middle text-white bg-opacity-80'>
-      <h1 className='font-bold py-4 text-3xl'> {isSignInForm ? 'Sign In' : 'Sign Up'} </h1>
-      {!isSignInForm && (<input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700 rounded-md' />)}
-      <input type='text' placeholder='Email or mobile number' className='p-4 my-4 w-full bg-gray-700 rounded-md' />
-      <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700 rounded-md' />
-      <button className='p-4 my-6 bg-red-700 w-full rounded-md'>{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
-      <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? 'New to Netfllix? Sign Up Now' : 'Already a user? Sign In Now'}</p>
-    </form>
+      <form onSubmit={(e) => e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 align-middle text-white bg-opacity-80">
+        <h1 className="font-bold py-4 text-3xl">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+          ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-md"
+          />
+        )}
+        <input
+        ref={email}
+          type="text"
+          placeholder="Email or mobile number"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <input
+        ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <p className='text-red-500 font-bold text-lg'>{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-md" onClick={handleButtonClick}>
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netfllix? Sign Up Now"
+            : "Already a user? Sign In Now"}
+        </p>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login
---
+++ src/utils/validate.js
@@ -0,0 +1,12 @@
+export const checkValidData = (email, password, name, isSignUpForm) => {
+    debugger
+    const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(password);
+    const isNameValid = /^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$/gim.test(name);
+
+    if(!isEmailValid) return "Email is invalid";
+    if(!isPasswordValid) return "Password is invalid"
+    if(!isNameValid && isSignUpForm) return "Name is invalid"
+
+    return null;
+}
