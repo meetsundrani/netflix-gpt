e1285d2724c4216a8052903f94944f419fabdd2a
Meet Sundrani <meetsundrani@gmail.com>
2025-01-04 19:08:14 +0530

    added signin

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/validate'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase"
 
 const Login = () => {
@@ -37,7 +37,18 @@ const Login = () => {
           // ..
         }); 
     } else {
-    
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + "-" + errorMessage);
+      });
     }
     
   }
